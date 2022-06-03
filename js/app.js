3fbb5dec26cdae2cb17a0fc0c1f940507aa4ec58
Issam <lands.of.48@gmail.com>
2022-06-03 19:06:48 +0300

    functionality done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 class Tweet {
-  constructor(author, content, id, retweeted, liked) {
+  constructor(author, content, id) {
     this.author = author;
     this.content = content;
     this.id = id;
+    this.liked = false;
   }
   getTweet() {
-    return this.author, this.content, this.id;
+    return this.author, this.content, this.id, this.liked;
   }
   setTweet(newAuthor, newContent, newId) {
     newAuthor.trim();
@@ -16,6 +17,12 @@ class Tweet {
     this.author = newAuthor;
     this.content = newContent;
     this.id = newId;
+  }
+  setLiked() {
+    this.liked = !this.liked;
+  }
+  getLiked() {
+    return this.liked;
   }
 }
 
@@ -38,47 +45,88 @@ form.addEventListener("submit", (event) => {
 
   refreshFeed();
 
-  // window.localStorage.setItem(newTweet.id, JSON.stringify(newTweet));
+  window.localStorage.setItem(newTweet.id, JSON.stringify(newTweet));
 });
 
 function createTweet(tweet) {
-  let tweetTemplate = `
-                  <li>
-                    <div class="tweet" id=${tweet.id}>
-                      <div class="user-information">
-                        <div class="user-profile">
-                          <img
-                            src="./images/user-icon.png"
-                            alt="userProfile"
-                            width="40"
-                            height="40"
-                          />
-                        </div>
-                        <div class="user-details">
-                          <div class="username">
-                            <h4 id="username">@${tweet.author}</h4>
-                          </div>
-                          <div class="elipses-icon">
-                            <button class="btn-options">
-                              <i class="fa-solid fa-ellipsis"></i>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="tweet-content">
-                        <p>
-                         ${tweet.content}
-                        </p>
-                        <div class="tweet-buttons">
-                        <button class="btn-tweet"><i class="fa-solid fa-heart "></i></button>
-                        <button onclick="isRetweeted(${tweet.id})" class="btn-tweet"><i class="fa-solid fa-retweet "></i></button>
-                        </div>
-                      </div>
-                      <br/>
-                      <hr class="h-line"/>
-                    </div>
-                  </li>
+  let tweetTemplate = "";
+  if (tweet.liked) {
+    tweetTemplate = `
+    <li>
+      <div class="tweet">
+        <div class="user-information">
+          <div class="user-profile">
+            <img
+              src="./images/user-icon.png"
+              alt="userProfile"
+              width="40"
+              height="40"
+            />
+          </div>
+          <div class="user-details">
+            <div class="username">
+              <h4 id="username">@${tweet.author}</h4>
+            </div>
+            <div class="elipses-icon">
+              <button class="btn-options">
+                <i class="fa-solid fa-ellipsis"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-content">
+          <p>
+           ${tweet.content}
+          </p>
+          <div class="tweet-buttons">
+          <button class="btn-tweet" onclick="isLiked(${tweet.id})"><i class="fa-solid fa-heart" style='color:red;'> </i></button>
+          <button onclick="isRetweeted(${tweet.id})" class="btn-tweet"><i class="fa-solid fa-retweet" ></i></button>
+          </div>
+        </div>
+        <br/>
+        <hr class="h-line"/>
+      </div>
+    </li>
     `;
+  } else {
+    tweetTemplate = `
+    <li>
+      <div class="tweet">
+        <div class="user-information">
+          <div class="user-profile">
+            <img
+              src="./images/user-icon.png"
+              alt="userProfile"
+              width="40"
+              height="40"
+            />
+          </div>
+          <div class="user-details">
+            <div class="username">
+              <h4 id="username">@${tweet.author}</h4>
+            </div>
+            <div class="elipses-icon">
+              <button class="btn-options">
+                <i class="fa-solid fa-ellipsis"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="tweet-content">
+          <p>
+           ${tweet.content}
+          </p>
+          <div class="tweet-buttons">
+          <button class="btn-tweet" onclick="isLiked(${tweet.id})"><i class="fa-solid fa-heart" style='color:black;'> </i></button>
+          <button onclick="isRetweeted(${tweet.id})" class="btn-tweet"><i class="fa-solid fa-retweet" ></i></button>
+          </div>
+        </div>
+        <br/>
+        <hr class="h-line"/>
+      </div>
+    </li>
+    `;
+  }
 
   tweetsList.insertAdjacentHTML("beforeend", tweetTemplate);
 }
@@ -90,6 +138,15 @@ function isRetweeted(tweetID) {
   refreshFeed();
 }
 
+function isLiked(tweetID) {
+  for (let i = 0; i < tweets.length; i++) {
+    if (tweets[i].id == tweetID) {
+      let fetchedTweet = tweets[i];
+      fetchedTweet.setLiked();
+    }
+  }
+  refreshFeed();
+}
 function getUniqueListBy(arr, key) {
   return [...new Map(arr.map((item) => [item[key], item])).values()];
 }
